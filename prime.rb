42c44c2d2471af5b989a9f833699768649457568
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-08-03 04:52:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(int)
+  divider = 1
+  arr = []
+  while divider < int-1 
+    divider += 1
+    arr.push(int % divider)
+  end
+    if int > 1
+      if arr.include?(0) 
+       false
+    else 
+      true
+    end
+  else 
+    false 
+  end
+end
